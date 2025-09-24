5e804b2877d369c286306c416524623d308d7767
Mathys Faget <mathys.faget.24@orange.fr>
2025-09-24 18:03:14 +0200

    Création nouvelle pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import './App.css';
-import CharactersPage from './pages/CharactersPage';
+import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router";
+import routes from './routes';
+
+// router to navigate through the app
+const router = createBrowserRouter(routes);
 
 function App() {
-  return (
-    <CharactersPage />
-  );
+    return (
+        <>
+            <RouterProvider router={router} />
+        </>
+    );
 }
 
 export default App;
